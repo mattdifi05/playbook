f7c50f26457f1e7ec9b8c7bdbae35aeed5f878f5
Matthew <95946096+mattdifi05@users.noreply.github.com>
2025-03-16 20:20:04 +0100

    Introducing LightOS

--- src/Executables/FINALIZE.cmd
+++ src/Executables/FINALIZE.cmd
@@ -19,23 +19,13 @@ rmdir /s /q "%ProgramW6432%\\PCHealthCheck" >NUL 2>nul
 "%ProgramFiles(x86)%\WindowsInstallationAssistant\Windows10UpgraderApp.exe" /SunValley /ForceUninstall >NUL 2>nul
 rmdir /s /q "%ProgramFiles(x86)%\WindowsInstallationAssistant" >NUL 2>nul
 
-if not defined w11 (
-	bcdedit /set description "ReviOS 10 %version%" >NUL 2>nul
-  reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "Model"  /t REG_SZ /d "ReviOS 10 %version%" /f >NUL 2>nul
-  reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "RegisteredOrganization" /t REG_SZ /d "ReviOS 10 %version%" /f >NUL 2>nul
-) else (
-	bcdedit /set description "ReviOS 11 %version%" >NUL 2>nul
-  reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "Model"  /t REG_SZ /d "ReviOS 11 %version%" /f >NUL 2>nul
-  reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "RegisteredOrganization" /t REG_SZ /d "ReviOS 11 %version%" /f >NUL 2>nul
-)
-
 @REM PowerShell -NonInteractive -NoLogo -NoP -C "Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod" >NUL 2>nul
 
 @REM echo Configuring power settings
 @REM powercfg /hibernate off
 @REM powercfg -restoredefaultschemes
 @REM powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 3ff9831b-6f80-4830-8178-736cd4229e7b
-@REM powercfg -changename 3ff9831b-6f80-4830-8178-736cd4229e7b "Revision - Ultra Performance" "Windows's Ultimate Performance with additional changes."
+@REM powercfg -changename 3ff9831b-6f80-4830-8178-736cd4229e7b "LightOS - Ultra Performance" "Windows's Ultimate Performance with additional changes."
 @REM powercfg -s 3ff9831b-6f80-4830-8178-736cd4229e7b
 @REM powercfg -setacvalueindex scheme_current sub_processor PERFINCPOL 2
 @REM powercfg -setacvalueindex scheme_current sub_processor PERFDECPOL 1
@@ -45,7 +35,7 @@ if not defined w11 (
 @REM powercfg -setacvalueindex scheme_current sub_processor CPMINCORES1 100
 @REM powercfg /setactive scheme_current
 
-@REM PowerShell -NonInteractive -NoLogo -NoP -C "& {$cpu = Get-CimInstance Win32_Processor; $cpuName = $cpu.Name; if ($cpu.Manufacturer -eq 'GenuineIntel') { if ($cpuName.Substring(0, 2) -eq 'In') { Write-Host 'Detected Intel CPU older than 10th generation.' } else { $cpuGen = [int]($cpuName.Substring(0, 2)); if ($cpuGen -gt 11) { Write-Host 'Optimizing Revision''s Ultra powerplan for 12th generation or later Intel CPUs'; powercfg -changename 3ff9831b-6f80-4830-8178-736cd4229e7b 'Revision - Ultra Performance' 'Windows''s Ultimate Performance with optimized settings for newer Intel CPUs.'; powercfg -s 3ff9831b-6f80-4830-8178-736cd4229e7b; powercfg -setacvalueindex scheme_current sub_processor HETEROPOLICY 0; powercfg -setacvalueindex scheme_current sub_processor SCHEDPOLICY 2; powercfg /setactive scheme_current }}};}"
+@REM PowerShell -NonInteractive -NoLogo -NoP -C "& {$cpu = Get-CimInstance Win32_Processor; $cpuName = $cpu.Name; if ($cpu.Manufacturer -eq 'GenuineIntel') { if ($cpuName.Substring(0, 2) -eq 'In') { Write-Host 'Detected Intel CPU older than 10th generation.' } else { $cpuGen = [int]($cpuName.Substring(0, 2)); if ($cpuGen -gt 11) { Write-Host 'Optimizing LightOS''s Ultra powerplan for 12th generation or later Intel CPUs'; powercfg -changename 3ff9831b-6f80-4830-8178-736cd4229e7b 'LightOS - Ultra Performance' 'Windows''s Ultimate Performance with optimized settings for newer Intel CPUs.'; powercfg -s 3ff9831b-6f80-4830-8178-736cd4229e7b; powercfg -setacvalueindex scheme_current sub_processor HETEROPOLICY 0; powercfg -setacvalueindex scheme_current sub_processor SCHEDPOLICY 2; powercfg /setactive scheme_current }}};}"
 
 echo Configuring tasks
 schtasks /change /tn "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem" /disable >NUL 2>nul
@@ -84,8 +74,6 @@ echo Configuring teredo
 netsh interface Teredo set state type=default >NUL 2>nul
 netsh interface Teredo set state servername=default >NUL 2>nul
 
-::https://github.com/meetrevision/playbook/issues/27
-::netsh int tcp set supplemental internet congestionprovider=bbr2 >NUL 2>nul
 
 echo Configuring Windows settings
 net accounts /maxpwage:unlimited
@@ -102,9 +90,6 @@ echo Configuring animations
 
 :: Breaks XboxGipSvc
 ::reg add "HKLM\SYSTEM\CurrentControlSet\Control" /v "SvcHostSplitThresholdInKB" /t REG_DWORD /d "4294967295" /f >NUL
-
-:: https://github.com/meetrevision/playbook/issues/15
-:: Updates root certificates
 
 ::echo Updating root certificates
 
